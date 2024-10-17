356dfe423079b5dde35635c9405903e180ef6d7f
h593265 <danield@live.no>
2024-10-17 18:02:55 +0200

    added loading to productpage, adn added padding for mobile

--- src/pages/productpage.js
+++ src/pages/productpage.js
@@ -13,49 +13,44 @@ import Sideslidemenu from '../components/sideslidemenu';
 import { useAuthContext } from '../context/authprovider';
 import ErrorPage from './page404';
 import { useSidebar } from '../context/sidebarcontext';
-import { config } from '../util/config';
-
-function ProductPage({ showpopup }) {
-  const product = useLocation();
+import {config} from '../util/config'
+function ProductPage({showpopup}) {
+  const  product  = useLocation();
   
   const { addToCart } = useCart();
-  const { userFavorites, guestFavorites, addToFavorite, removeFromFavorite } = useFavorite();
+  const { userFavorites, guestFavorites,  addToFavorite, removeFromFavorite} = useFavorite();
   const { user } = useAuthContext();
-  const { sidebarOpen, openSidebar } = useSidebar();
-  
+  const { sidebarOpen, openSidebar} = useSidebar();
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true); // Loading state
   const [showMore, setShowMore] = useState(false);
   const [width, setWidth] = useState(0);
   const [liked, setLiked] = useState(false);
   
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true); // Start loading
-
+      
       try {
         let response;
-        const data = product.pathname.split("/");
-        const idname = data[data.length - 1];
-        response = await fetch(`${config.API_URL}/products/getbytitle?idname=${encodeURIComponent(idname)}`);
+          const data = product.pathname.split("/");
+         const idname = data[data.length-1]
+          response = await fetch(`${config.API_URL}/products/getbytitle?idname=${encodeURIComponent(idname)}`);
         
         if (!response.ok) {
           throw new Error('Failed to fetch products');
         }
-        
         const jsonData = await response.json();
         setData(jsonData);
       } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false); // End loading
-      }
+        
+        console.log(error)
+      } 
     };
-
-    setShowMore(false);
+    setShowMore(false)
     fetchData();
   }, [product]);
   
+
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [data]);
@@ -75,14 +70,21 @@ function ProductPage({ showpopup }) {
 
   useEffect(() => {
     function checkLiked() {
-      if (user) {
-        setLiked(userFavorites.some((item) => item.id === data?.id));
-      } else {
-        setLiked(guestFavorites.some((item) => item.id === data?.id));
+      
+      if(user) {
+       
+        setLiked(userFavorites.some((item) => item.id === data?.id))
+      } else{
+        
+        setLiked(guestFavorites.some((item) => item.id === data?.id))
+       
       }
+      
     }
 
-    checkLiked();
+    
+
+    checkLiked()
   }, [guestFavorites, userFavorites, data]);
 
   const handleBuyButton = () => {
@@ -90,27 +92,170 @@ function ProductPage({ showpopup }) {
     openSidebar("Handlekurv", data);
   };
 
+
+
   const handleFavoriteClick = async (event) => {
     event.stopPropagation();
   
     if (liked) {
+      
       await removeFromFavorite(data.id);
       setLiked(false);
     } else {
+      
       await addToFavorite(data.id);
       setLiked(true);
     }
   };
+  
 
   const bigLayout = () => (
     <div className="productpage-flex">
-      {/* Big layout content */}
+      <div className="productpage-image-container">
+        <img src={data.image} alt={data.title} />
+      </div>
+      <div className="productpage-text-section">
+        <div className="productpage-title">{data.title}</div>
+        <div className="productpage-author">{data.author}</div>
+        {data.description && data.category.split("/")[0] === "boker" ? (<div
+          className={
+            showMore
+              ? 'productpage-description-showmore'
+              : 'productpage-description-showless'
+          }
+        >
+          {data.description }
+          <div className={showMore ? 'showmore' : 'showless'}>
+           
+          </div>
+        
+        </div>) : (data.description) }
+        {
+          data.category.split("/")[0] === "boker"&&
+          <div className="showmore-text" onClick={() => setShowMore(!showMore)}>
+             {showMore ? 'Vis mindre' : 'Vis mer'}
+            </div>}
+        <div className='productpage-info-section'>
+        <div className='productpage-product-info'>Produktegenskaper <div className='productpage-arrow'>&#62;</div></div>
+        <div className='productpage-product-info'>Kundeanmeldelser <div className='productpage-arrow'>&#62;</div></div>
+        <div className='productpage-product-info'>Frakt og levering <div className='productpage-arrow'>&#62;</div></div>
+      </div>
+      </div>
+      <div className="productpage-interactive-section">
+        <div className="productpage-interactive-section-container">
+          <div className="productpage-interactive-stars-flex">
+            <div className="productpage-favorite" onClick={handleFavoriteClick}>
+              {liked ? (
+                <HeartIconFilled style={{stroke:"red", fill:"red"}} />
+              ) : (
+                <HeartIconOutline  />
+              )}
+            </div>
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+          </div>
+          <div className="productpage-price">{data.price},-</div>
+          <button className="productpage-buybutton" onClick={handleBuyButton}>
+            Legg i handlekurven
+          </button>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>På nettlager. Sendes innen 1-2
+            virkedager
+          </div>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>Gratis frakt på ordre fra 299,-
+          </div>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>Bytt i 200 butikker
+          </div>
+          <div style={{ height: '10px' }}></div>
+          <button className="productpage-buybutton" onClick={() => {}}>
+            Klikk og hent
+          </button>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>På lager i 110 butikker
+          </div>
+        </div>
+      </div>
     </div>
   );
 
   const smallLayout = () => (
     <div className="productpage-flex-small">
-      {/* Small layout content */}
+      <div className="productpage-image-container">
+        <img src={data.image} alt={data.title} />
+      </div>
+      <div className="productpage-title">{data.title}</div>
+      <div className="productpage-author">{data.author}</div>
+      <div style={{ height: '40px' }}></div>
+      <div className="productpage-interactive-section" style={{ width: '111%' }}>
+        <div className="productpage-interactive-section-container">
+          <div className="productpage-interactive-stars-flex">
+            <div className="productpage-favorite" onClick={handleFavoriteClick}>
+            {liked ? (
+                <HeartIconFilled style={{stroke:"red", fill:"red"}} />
+              ) : (
+                <HeartIconOutline  />
+              )}
+            </div>
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+            <StarIcon />
+          </div>
+          <div className="productpage-price">{data.price},-</div>
+          <button className="productpage-buybutton" onClick={handleBuyButton}>
+            Legg i handlekurven
+          </button>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>På nettlager. Sendes innen 1-2
+            virkedager
+          </div>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>Gratis frakt på ordre fra 299,-
+          </div>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>Bytt i 200 butikker
+          </div>
+          <div style={{ height: '10px' }}></div>
+          <button className="productpage-buybutton" onClick={() => {}}>
+            Klikk og hent
+          </button>
+          <div className="productpage-interactive-medal">
+            <div className="circle-green"></div>På lager i 110 butikker
+          </div>
+        </div>
+      </div>
+      <div style={{ height: '40px' }}></div>
+      {data.description && data.category.split("/")[0] === "boker" ? (<div
+          className={
+            showMore
+              ? 'productpage-description-showmore'
+              : 'productpage-description-showless'
+          }
+        >
+          {data.description }
+          <div className={showMore ? 'showmore' : 'showless'}>
+           
+          </div>
+        
+        </div>) : (data.description) }
+
+        {
+          data.category.split("/")[0] === "boker"&&
+          <div className="showmore-text" onClick={() => setShowMore(!showMore)}>
+             {showMore ? 'Vis mindre' : 'Vis mer'}
+            </div>}
+      
+      <div className='productpage-info-section'>
+        <div className='productpage-product-info'>Produktegenskaper <div className='productpage-arrow'>&#62;</div></div>
+        <div className='productpage-product-info'>Kundeanmeldelser <div className='productpage-arrow'>&#62;</div></div>
+        <div className='productpage-product-info'>Frakt og levering <div className='productpage-arrow'>&#62;</div></div>
+      </div>
     </div>
   );
 
