b558f8ca1025e2f27ef76920d5d2c9caf38880bf
Daniel Endresen <danield@live.no>
2025-12-05 11:10:49 +0100

    Update API URL to norli-clone-api-1.onrender.com

--- src/util/config.js
+++ src/util/config.js
@@ -1,9 +1,9 @@
 const dev = {
-    API_URL: 'https://your-render-url.onrender.com',
+    API_URL: 'https://norli-clone-api-1.onrender.com',
   };
   
   const prod = {
-    API_URL: 'https://your-render-url.onrender.com',
+    API_URL: 'https://norli-clone-api-1.onrender.com',
     DOMAIN_URL: 'https://norli-clone.onrender.com'
   };
 
